3344c2683b8224e870a93c484d97b0328848eea6
Georgii Ishehnko <metalkint@gmail.com>
2019-08-06 23:31:11 +0300

    fix path

--- src/store/actions/game-board/units/index.js
+++ src/store/actions/game-board/units/index.js
@@ -1,4 +1,4 @@
-import * as actionTypes from './actionTypes';
+import * as actionTypes from './action-types';
 
 export const removeUnit = (cellId, unitIndex) => ({
   type: actionTypes.REMOVE_UNIT,
